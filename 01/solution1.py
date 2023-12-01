ece64ebde96255aff54f01a275edbd110616167d
Alex Blackmore <ablackmore113@gmail.com>
2023-12-01 21:12:33 +1100

    day 1

--- 01/solution1.py
+++ 01/solution1.py
@@ -2,5 +2,10 @@
 from functools import reduce, cache
 from itertools import groupby
 
+def calibration(s):
+    n = list(filter(lambda x : x in ["0","1","2","3","4","5","6","7","8","9"], s))
+    return int(n[0] + n[-1])
+
 with open("input.txt") as file:
-    ...
+    lines = file.read().split("\n")[:-1]
+    print(sum(list(map(calibration, lines))))
--- 01/solution2.py
+++ 01/solution2.py
@@ -2,5 +2,28 @@
 from functools import reduce, cache
 from itertools import groupby
 
+literal_nums = {
+    "zero": "0",
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9"
+}
+
+def calibration(s):
+    n = []
+    for i in range(len(s)):
+        if s[i] in ["0","1","2","3","4","5","6","7","8","9"]:
+            n.append(s[i])
+        else:
+            [n.append(literal_nums[word]) for word in literal_nums if s[i:i+len(word)] == word]
+    return int(n[0] + n[-1])
+
 with open("input.txt") as file:
-    ...
+    lines = file.read().split("\n")[:-1]
+    print(sum(list(map(calibration, lines))))
