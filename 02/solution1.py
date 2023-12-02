d4420c4447cffc3326b195480a0838b60c6f977f
Alex Blackmore <ablackmore113@gmail.com>
2023-12-02 21:18:17 +1100

    day 2 solutions

--- 02/solution1.py
+++ 02/solution1.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
+allowed = {"red": 12, "green": 13, "blue": 14}
+
+def valid(s):
+    nopunc = "".join([c for c in s if c not in [':', ',', ';']])
+    pairs = list(zip(*[iter(nopunc.split())] * 2))[1:]
+    return not any(map(lambda x : int(x[0]) > allowed[x[1]], pairs))
+
 with open("input.txt") as file:
-    ...
+    lines = file.read().strip().split("\n")
+    print(sum([int(l.split()[1][:-1]) for l in lines if valid(l)]))
--- 02/solution2.py
+++ 02/solution2.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
+import functools as f
+def power(s):
+    min = {"red": 0, "green": 0, "blue": 0}
+    nopunc = "".join([c for c in s if c not in [':', ',', ';']])
+    pairs = list(zip(*[iter(nopunc.split())] * 2))[1:]
+    for x in pairs: 
+        if int(x[0]) > min[x[1]]: 
+            min[x[1]] = int(x[0])
+    return f.reduce(lambda x, y : x * int(min[y]), min, 1)
 
 with open("input.txt") as file:
-    ...
+    lines = file.read().strip().split("\n")
+    print(sum(map(power, lines)))
