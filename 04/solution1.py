d3648080880aa30b309b2e943929ed2f195f7fa4
Alex Blackmore <ablackmore113@gmail.com>
2023-12-05 07:07:19 +1100

    day 5

--- 04/solution1.py
+++ 04/solution1.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
+def score(line):
+    [c1, c2] = line.split('|')
+    c1 = c1.split(':')[1].strip().split()
+    c2 = c2.strip().split()
+    return round(2 ** (len(list(filter(lambda x : x in c1, c2))) - 1))
+
 with open("input.txt") as file:
-    ...
+    lines = file.read().strip().split("\n")
+    print(sum(map(score, lines)))
--- 04/solution2.py
+++ 04/solution2.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
+import functools as ft
+scratchie = []
+
+def calculate(xs, x=0):
+    if xs == []: return x
+    for i in range(xs[0][0]):
+        xs[i + 1][1] += xs[0][1]
+    return calculate(xs[1:], x + xs[0][1])
+
+def score(line):
+    n = int(line.split(':')[0].split()[1])
+    [c1, c2] = line.split('|')
+    c1 = c1.split(':')[1].strip().split()
+    c2 = c2.strip().split()
+    matches = len(list(filter(lambda x : x in c1, c2)))
+    scratchie.append([matches, 1])
 
 with open("input.txt") as file:
-    ...
+    lines = file.read().strip().split("\n")
+    for line in lines: score(line)
+    print(calculate(scratchie))
