f539298323fa1a9fe91f3f5da1fbf7069d602a15
Alex Blackmore <ablackmore113@gmail.com>
2023-12-05 06:46:27 +1100

    day 3

--- 03/solution1.py
+++ 03/solution1.py
@@ -1,4 +1,32 @@
 #!/usr/bin/env python3
+import functools as ft
+import itertools as it
+
+ns = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+check = [-1, 1, 0]
+
+def valid(lines, i, nu):
+    bad = ns + ['.']
+    for j in nu:
+        for c1 in check:
+            for c2 in check:
+                if lines[i + c1][j + c2] not in bad: return True
+    return False
+
+def numbers(lines, i):
+    line = lines[i]
+    digits = list(filter(lambda x : x[1] in ns, enumerate(line)))
+    nums = []
+    for di, _ in digits:
+        if nums == []: nums = [[di]]
+        elif nums[-1][-1] != di - 1: nums.append([di])
+        else: nums[-1].append(di)
+    nums = filter(ft.partial(valid, lines, i), nums)
+    return sum(map(lambda xs : int("".join(map(lambda x : lines[i][x], xs))), nums))
 
 with open("input.txt") as file:
-    ...
+    lines = file.read().strip().split("\n")
+    lines = ['.' * len(lines[0])]\
+        + list(map(lambda x : '.' + x + '.', lines))\
+        + ['.' * len(lines[0])]
+    print(sum(map(ft.partial(numbers, lines), range(len(lines)))))
--- 03/solution2.py
+++ 03/solution2.py
@@ -1,4 +1,49 @@
 #!/usr/bin/env python3
+import functools as ft
+import itertools as it
+
+ns = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+check = [-1, 1, 0]
+
+def adj(i, j):
+    ret = []
+    for c1 in check:
+        for c2 in check:
+            if c1 or c2:
+                ret.append((i + c1, j + c2))
+    return ret
+
+def rangeof(lines, i, j):
+    if lines[i][j] not in ns:
+        return None
+    l = j
+    while lines[i][l] in ns: l -= 1
+    l += 1
+    u = j
+    while lines[i][u] in ns: u += 1
+    u -= 1
+    return (i, l, u)
+
+def rangetonum(lines, i, l, u):
+    s = ""
+    for k in range(l, u + 1): s += lines[i][k]
+    return int(s)
+
+def total(lines, i):
+    ttl = 0
+    gears = [x[0] for x in enumerate(lines[i]) if x[1] == "*"]
+    for gear in gears:
+        ranges = set()
+        for cell in adj(i, gear):
+            if lines[cell[0]][cell[1]] in ns and rangeof(lines, cell[0], cell[1]):
+                ranges.add(rangeof(lines, cell[0], cell[1]))
+        if len(ranges) == 2:
+            ttl += ft.reduce(lambda x, y : x * y, map(lambda x : rangetonum(lines, *x), ranges))
+    return ttl
 
 with open("input.txt") as file:
-    ...
+    lines = file.read().strip().split("\n")
+    lines = ['.' * len(lines[0])]\
+        + list(map(lambda x : '.' + x + '.', lines))\
+        + ['.' * len(lines[0])]
+    print(sum(map(ft.partial(total, lines), range(len(lines)))))
