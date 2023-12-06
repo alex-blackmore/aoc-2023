93a261b569040387b2effa46adacc2d4bfc4764d
Alex Blackmore <ablackmore113@gmail.com>
2023-12-06 20:19:02 +1100

    day 6

--- 06/solution1.py
+++ 06/solution1.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
+import math as m
+import functools as ft
+def count_sols(duration, record):
+    minhold = (duration - m.sqrt((duration ** 2) - (4 * record))) / 2
+    maxhold = (duration + m.sqrt((duration ** 2) - (4 * record))) / 2
+    adjust = 0
+    if minhold == m.floor(minhold): adjust -= 1
+    if maxhold == m.floor(maxhold): adjust -= 1
+    return (m.floor(maxhold) - m.ceil(minhold) + 1 + adjust)
 
 with open("input.txt") as file:
-    ...
+    lines = file.read().strip().split("\n")
+    times = list(map(int, lines[0].split(':')[1].strip().split()))
+    records = list(map(int, lines[1].split(':')[1].strip().split()))
+    races = list(zip(times, records))
+    print(ft.reduce(lambda x, y : x * y, map(lambda x : count_sols(*x), races), 1))
--- 06/solution2.py
+++ 06/solution2.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
+import math as m
+import functools as ft
+def count_sols(duration, record):
+    minhold = (duration - m.sqrt((duration ** 2) - (4 * record))) / 2
+    maxhold = (duration + m.sqrt((duration ** 2) - (4 * record))) / 2
+    adjust = 0
+    if minhold == m.floor(minhold): adjust -= 1
+    if maxhold == m.floor(maxhold): adjust -= 1
+    return (m.floor(maxhold) - m.ceil(minhold) + 1 + adjust)
 
 with open("input.txt") as file:
-    ...
+    lines = file.read().strip().split("\n")
+    time = int(ft.reduce(lambda x, y : x + y, filter(str.isdigit, lines[0].split(':')[1].strip())))
+    record = int(ft.reduce(lambda x, y : x + y, filter(str.isdigit, lines[1].split(':')[1].strip())))
+    print(count_sols(time, record))
