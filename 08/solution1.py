abd1114506fe9b2cb33f37d711d0975cdd65b6c8
Alex Blackmore <ablackmore113@gmail.com>
2023-12-08 21:34:47 +1100

    day 8

--- 08/solution1.py
+++ 08/solution1.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
+import re
+import functools as ft
+
+cur_node = 'AAA'
+node_map = {}
+counter = 0
+
+def loop(instructions):
+    global cur_node, node_map, counter
+    for direction in instructions: 
+        cur_node = node_map[cur_node][0] if direction == 'L' else node_map[cur_node][1]
+        counter += 1
+
+def concat_map(mapping, elem):
+    mapping[elem[0]] = [elem[1], elem[2]]
+    return mapping
 
 with open("input.txt") as file:
-    ...
+    instructions, _, *nodes = file.read().strip().split("\n")
+    nodes = list(map(lambda x : re.sub(r'([A-Z]+) = \(([A-Z]+), ([A-Z]+)\)', r'\1 \2 \3', x).split(), nodes))
+    nodes = ft.reduce(lambda x, y : concat_map(x, y), nodes, node_map)
+    while cur_node != 'ZZZ': loop(instructions)
+    print(counter)
--- 08/solution2.py
+++ 08/solution2.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
+import re
+import functools as ft
+import math as m
+
+cur_nodes = []
+node_map = {}
+node_tracker = {}
+counter = 1
+
+def loop(instructions):
+    global cur_nodes, node_map, counter
+    for direction in instructions: 
+        for i in range(len(cur_nodes)):
+            cur_nodes[i] = node_map[cur_nodes[i]][0] if direction == 'L' else node_map[cur_nodes[i]][1]
+            if cur_nodes[i][2] == 'Z' and not node_tracker[cur_nodes[i]]: 
+                node_tracker[cur_nodes[i]] = counter
+        counter += 1
+
+def concat_map(mapping, elem):
+    mapping[elem[0]] = [elem[1], elem[2]]
+    return mapping
 
 with open("input.txt") as file:
-    ...
+    instructions, _, *nodes = file.read().strip().split("\n")
+    nodes = list(map(lambda x : re.sub(r'([A-Z]+) = \(([A-Z]+), ([A-Z]+)\)', r'\1 \2 \3', x).split(), nodes))
+    nodes = ft.reduce(lambda x, y : concat_map(x, y), nodes, node_map)
+    cur_nodes = [node for node in nodes if node[2] == 'A']
+    for node in nodes: 
+        if node[2] == 'Z': node_tracker[node] = None
+    # get loop lengths
+    while any(map(lambda k : node_tracker[k] == None, node_tracker)): loop(instructions)
+    # this only works because the input is friendly
+    print(m.lcm(*node_tracker.values()))
