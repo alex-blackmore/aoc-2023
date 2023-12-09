27eaff0074008cc07cc40530c049e0e959b47263
Alex Blackmore <ablackmore113@gmail.com>
2023-12-09 21:26:27 +1100

    day 9

--- 09/solution1.py
+++ 09/solution1.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
+import functools as ft
+
+def extrapolate(sequence):
+    return list(map(lambda i : sequence[i] - sequence[i - 1], range(1, len(sequence))))
+
+def unextrapolate(known, unknown):
+    return unknown + [unknown[-1] + known[-1]]
 
 with open("input.txt") as file:
-    ...
+    histories = [[list(map(int, h.split()))] for h in file.read().strip().split("\n")]
+    sum = 0
+    for history in histories: 
+        while any(history[-1]): 
+            history.append(extrapolate(history[-1]))
+        lr = history.pop() + [0]
+        history.reverse()
+        sum += ft.reduce(unextrapolate, history, lr)[-1]
+    print(sum)
