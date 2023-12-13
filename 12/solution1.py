8db16cf28e8356d98bc49ad83dbd691f8c0e47c1
Alex Blackmore <ablackmore113@gmail.com>
2023-12-13 21:06:18 +1100

    day 12

--- 12/solution1.py
+++ 12/solution1.py
@@ -1,4 +1,36 @@
 #!/usr/bin/env python3
+import functools as ft
+
+@ft.cache
+def solve(start, s, r, springs, records):
+    # base cases
+    if r >= len(records): return 1 if not any(map(lambda x : x == '#', springs[s:])) else 0
+    if s >= len(springs): return 0
+
+    # can't use    
+    if s + records[r] > len(springs): return 0
+
+    for i in range(s, s + records[r]):
+        if springs[i] == '.':
+            for j in range(s, i):
+                if springs[j] == '#':
+                    return 0
+            return solve(True, i + 1, r, springs, records)
+
+    if s + records[r] < len(springs) and springs[s + records[r]] == '#':
+        if springs[s] == '#': return 0
+        return solve(True, s + 1, r, springs, records)
+
+    if not start: 
+        if springs[s] == '#': return 0
+        return solve(True, s + 1, r, springs, records)
+
+    # must use
+    if springs[s] == '#': return solve(False, s + records[r], r + 1, springs, records)
+    
+    # may use
+    return solve(False, s + records[r], r + 1, springs, records) + solve(True, s + 1, r, springs, records)
 
 with open("input.txt") as file:
-    ...
+    problems = [(tuple(x.split()[0]), tuple(map(int, x.split()[1].split(',')))) for x in file.read().strip().split("\n")]
+    print(sum(map(lambda ps : solve(True, 0, 0, *ps), problems)))
--- 12/solution2.py
+++ 12/solution2.py
@@ -1,4 +1,46 @@
 #!/usr/bin/env python3
+import functools as ft
+
+@ft.cache
+def solve(start, s, r, springs, records):
+    # base cases
+    if r >= len(records): 
+        return 1 if not any(map(lambda x : x == '#', springs[s:])) else 0
+
+    if s >= len(springs): 
+        return 0
+
+    # can't use    
+    if s + records[r] > len(springs): 
+        return 0
+
+    for i in range(s, s + records[r]):
+        if springs[i] == '.':
+            for j in range(s, i):
+                if springs[j] == '#':
+                    return 0
+            return solve(True, i + 1, r, springs, records)
+
+    if s + records[r] < len(springs) and springs[s + records[r]] == '#':
+        # must place but cannot 
+        if springs[s] == '#': return 0
+        return solve(True, s + 1, r, springs, records)
+
+    if not start: 
+        # must use but can't
+        return solve(True, s + 1, r, springs, records)
+
+    # must use
+    if springs[s] == '#': 
+        return solve(False, s + records[r], r + 1, springs, records)
+
+    # may use
+    return solve(False, s + records[r], r + 1, springs, records) + solve(True, s + 1, r, springs, records)
+
+def unfold(problem):
+    return (((problem[0] + ("?",)) * 5)[:-1], problem[1] * 5)
 
 with open("input.txt") as file:
-    ...
+    problems = [(tuple(x.split()[0]), tuple(map(int, x.split()[1].split(',')))) for x in file.read().strip().split("\n")]
+    problems = list(map(unfold, problems))
+    print(sum(map(lambda ps : solve(True, 0, 0, *ps), problems)))
