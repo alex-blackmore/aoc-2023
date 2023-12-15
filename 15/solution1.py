8351199bb28d25f81aa9bf57d3a1ae1e52aab2c4
Alex Blackmore <ablackmore113@gmail.com>
2023-12-15 18:33:55 +1100

    day 15

--- 15/solution1.py
+++ 15/solution1.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+import functools as ft
+def hash(cur, new):
+    return ((cur + ord(new)) * 17) % 256
 
 with open("input.txt") as file:
-    ...
+    codes = [list(x) for x in file.read().split(",")]
+    print(sum([ft.reduce(hash, code, 0) for code in codes]))
--- 15/solution2.py
+++ 15/solution2.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
+import functools as ft
+import re
+
+def hash(cur, new):
+    return ((cur + ord(new)) * 17) % 256
+
+boxes = [[] for _ in range(256)]
 
 with open("input.txt") as file:
-    ...
+    for instruction in file.read().strip().split(","):
+        label, protocol = re.match(r'([a-z]+)([=-][1-9]?)', instruction).groups()
+        box = ft.reduce(hash, label, 0)
+        match protocol[0]:
+            case '-':
+                boxes[box] = [x for x in boxes[box] if x[0] != label]
+            case '=':
+                try:
+                    i = [x[0] for x in boxes[box]].index(label)
+                    boxes[box][i] = (boxes[box][i][0], int(protocol[1]))
+                except:
+                    boxes[box].append((label, int(protocol[1])))
+
+    print(sum([sum([bnum * lnum * lens[1] for lnum, lens in enumerate(box, 1)]) for bnum, box in enumerate(boxes, 1)]))
