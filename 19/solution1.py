94056624bd53af0aaa40d1455304d3c7feb18efc
Alex Blackmore <ablackmore113@gmail.com>
2023-12-19 23:44:20 +1100

    day 19

--- 19/solution1.py
+++ 19/solution1.py
@@ -1,4 +1,38 @@
 #!/usr/bin/env python3
+import re
+
+type_to_index = {'x': 0, 'm': 1, 'a': 2, 's': 3}
+
+def process(part, name, rules):
+    for rule in rules[name]:
+        if matching(rule, part):
+            result = rule.split(':')[1]
+            break
+    else: 
+        result = rule
+
+    if result == 'A': 
+        return sum(part)
+    if result == 'R': 
+        return 0
+
+    return process(part, result, rules)
+
+def matching(rule, part):
+    if len(rule) == 1 or rule[1] not in ['<', '>']: 
+        return False
+
+    if '>' in rule:
+        return part[type_to_index[rule[0]]] > int(rule.split(':')[0].split('>')[1])
+    else:
+        return part[type_to_index[rule[0]]] < int(rule.split(':')[0].split('<')[1])
 
 with open("input.txt") as file:
-    ...
+    rs, parts = file.read().split("\n\n")
+    rs = [("".join(l.split('{')[0]), l.split('{')[1].strip('}').split(",")) for l in rs.strip().split("\n")]
+    rules = {}
+    parts = [[int(x) for x in re.findall(r'\d+',l)] for l in parts.strip().split("\n")]
+    for name, r in rs: 
+        rules[name] = r
+
+    print(sum([process(part, "in", rules) for part in parts]))
--- 19/solution2.py
+++ 19/solution2.py
@@ -1,4 +1,69 @@
 #!/usr/bin/env python3
 
+import re
+import functools as ft
+
+type_to_index = {'x': 0, 'm': 1, 'a': 2, 's': 3}
+
+def map_ratings(rating, rules, name, rejected, accepted):
+    remaining = rating[:]
+    for rule in rules[name]:
+        if is_final(rule):
+            if rule == 'A':
+                accepted.append(remaining)
+            elif rule == 'R':
+                rejected.append(remaining)
+            else:
+                map_ratings(remaining, rules, rule, rejected, accepted)
+        else:
+            result, matching, remaining = split_ratings(rule, remaining)
+            if result == 'A':
+                accepted.append(matching)
+            elif result == 'R':
+                rejected.append(matching)
+            else:
+                map_ratings(matching, rules, result, rejected, accepted)
+        
+def split_ratings(rule, rating):
+    splitpoint = int(re.match(r'[xmas][<>](\d+)', rule).groups()[0])
+    rtype = rule[0]
+    result = rule.split(':')[1]
+    matching = rating[:]
+    remaining = rating[:]
+
+    if '>' in rule:
+        lower, upper = rating[type_to_index[rtype]]
+        if splitpoint < lower:
+            remaining[type_to_index[rtype]] = []
+        elif splitpoint >= upper:
+            matching[type_to_index[rtype]] = []
+        else:
+            remaining[type_to_index[rtype]] = [lower, splitpoint]
+            matching[type_to_index[rtype]] = [splitpoint + 1, upper]
+    else:
+        (lower, upper) = rating[type_to_index[rtype]]
+        if splitpoint > upper:
+            remaining[type_to_index[rtype]] = []
+        elif splitpoint <= lower:
+            matching[type_to_index[rtype]] = []
+        else:
+            remaining[type_to_index[rtype]] = [splitpoint, upper]
+            matching[type_to_index[rtype]] = [lower, splitpoint - 1]
+    return result, matching, remaining
+
+def is_final(rule):
+    return '>' not in rule and '<' not in rule
+
 with open("input.txt") as file:
-    ...
+    rs, _ = file.read().split("\n\n")
+    rs = [("".join(l.split('{')[0]), l.split('{')[1].strip('}').split(",")) for l in rs.strip().split("\n")]
+    rules = {}
+    rating = [[1, 4000], [1, 4000], [1, 4000], [1, 4000]]
+    rejected = []
+    accepted = []
+    for name, r in rs: 
+        rules[name] = r
+
+    map_ratings(rating, rules, "in", rejected, accepted)
+
+    print(sum([ft.reduce(lambda x, y : x * y, [rng[1] - rng[0] + 1 for rng in rating]) for rating in accepted]))
