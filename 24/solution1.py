d6f875a2e62a58037562c41966fc66dccbc3088f
Alex Blackmore <ablackmore113@gmail.com>
2023-12-24 20:29:19 +1100

    day 24 part 1

--- 24/solution1.py
+++ 24/solution1.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
+import re
+import itertools as it
+
+LOWER_BOUND = 200000000000000
+UPPER_BOUND = 400000000000000
+
+def intersection(hailstone1, hailstone2):
+    x1, y1, _, dx1, dy1, _ = hailstone1
+    x2, y2, _, dx2, dy2, _ = hailstone2 
+    gradient1 = dy1 / dx1
+    gradient2 = dy2 / dx2
+    if gradient1 == gradient2: return False
+    x = (y2 - y1 + x1 * (gradient1) - x2 * (gradient2)) / (gradient1 - gradient2)
+    y = (gradient1) * (x - x1) + y1
+    time1 = (x - x1) / dx1
+    time2 = (x - x2) / dx2
+    if time1 < 0 or time2 < 0: return False
+    if y < LOWER_BOUND or y > UPPER_BOUND: return False
+    if x < LOWER_BOUND or x > UPPER_BOUND: return False
+    return True
 
 with open("input.txt") as file:
-    ...
+    hailstones = [[int(x) for x in re.findall('-?\d+', line)] for line in file.read().strip().split("\n")]
+    print(len([pair for pair in it.combinations(hailstones, 2) if intersection(*pair)]))
--- 24/solution2.py
+++ 24/solution2.py
@@ -1,4 +1,40 @@
 #!/usr/bin/env python3
+import re
+import itertools as it
+
+# given an intital position, is it possible to test whether some velocity exists so that all hailstones can be striked?
+# yes. given the closest and second closest hailstone's velocity, there is only one potential velocity
+
+# probably not possible to brute force
+
+# if any two lines from the input intersect, then they define a plane on which my throw my lie
+# if any other two lines intersect on a different plane then they fully define the throw as the intersection of two planes
+
+def cross(a, b):
+    c = [a[1]*b[2] - a[2]*b[1],
+         a[2]*b[0] - a[0]*b[2],
+         a[0]*b[1] - a[1]*b[0]]
+
+    return c
+
+def intersection(hailstone1, hailstone2):
+    normal = cross(hailstone1[3:], hailstone2[3:])
+    point = hailstone1[:3]
+    x, y, z = hailstone2[:3]
+    result = normal[0] * (point[0] - x) + normal[1] * (point[1] - y) + normal[2] * (normal[2] - z)
+    if result != 0:
+        return False
+    return True
+
+def parallel(hailstone1, hailstone2):
+    dx1, dy1, dz1 = hailstone1[3:]
+    dx2, dy2, dz2 = hailstone2[3:]
+    return dx1 / dx2 == dy1 / dy2 and  dy1 / dy2 == dz1 / dz2
 
 with open("input.txt") as file:
-    ...
+    hailstones = [[int(x) for x in re.findall('-?\d+', line)] for line in file.read().strip().split("\n")]
+    for pair in it.combinations(hailstones, 2):
+        if intersection(*pair):
+            print(pair)
+        if parallel(*pair):
+            print(pair)
